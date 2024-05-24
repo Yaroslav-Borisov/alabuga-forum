74a550cadf263a5932973de8e693ee181bec9a43
Yaroslav-Borisov <yarpolar@gmail.com>
2024-05-24 12:19:05 +0300

    добавил visual hidden

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -16,12 +16,37 @@ const Main = () => {
         var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/180000|0);
         var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
         })(window,document,'https://team.alabuga.ru/upload/crm/form/loader_86_k6idnt.js');
-      </script>`
+      </script>
+      `
     );
   }, []);
 
+  // useEffect(() => {
+  // const form = document.querySelector('.b24-form');
+  // if (form !== null) {
+  //   console.log(form);
+  // }
+  // postscribe(
+  //   '#main',
+  //   `
+  //     <script>
+  //     const form = document.querySelector('.b24-form')
+  //     if(form) {
+  //       const allFields = [...document.querySelectorAll('.b24-form-field')]
+  //       console.log(allFields)
+  //       allFields.forEach((item, index) => {
+  //         if(index <= 4 ) {
+  //           allFields[index].classList.add('visually-hidden');
+  //         }
+  //       })
+  //     }
+  //     </script>
+  //     `
+  // );
+  // }, [isFormRendered]);
+
   return (
-    <main className={styles.main}>
+    <main className={styles.main} id="main">
       <Home />
       <Program />
       <div className={styles.terms__big}>
@@ -32,7 +57,11 @@ const Main = () => {
       </div>
       <div className={styles.application} id="application">
         <h2 className={styles.application__title}>Заявка для участия</h2>
-        <div id="form"></div>
+        <div className={styles.form__wrapper}>
+          <div className={styles.form}>
+            <div id="form"></div>
+          </div>
+        </div>
       </div>
     </main>
   );
